6e760d3634383d9e1af4df45bd4702e05f3bb19f
Erik Hanson <erik@eahanson.com>
2019-06-18 07:07:10 -0700

    Extract Browser.simulate_click

--- lib/click/browser.ex
+++ lib/click/browser.ex
@@ -2,7 +2,9 @@ defmodule Click.Browser do
   alias ChromeRemoteInterface.PageSession
   alias ChromeRemoteInterface.RPC
   alias ChromeRemoteInterface.Session
+  alias Click.Chrome
   alias Click.DomNode
+  alias Click.Quad
 
   def new(base_url, opts \\ []) do
     with node <- %DomNode{base_url: base_url, id: nil, pid: nil},
@@ -11,6 +13,74 @@ defmodule Click.Browser do
          {:ok, node} <- navigate(node, "/"),
          {:ok, node} <- get_document(node) do
       node
+    end
+  end
+
+  #
+
+  def get_document(%DomNode{pid: pid} = node) do
+    with {:ok, %{"result" => %{"root" => %{"nodeId" => id}}}} <- RPC.DOM.getDocument(pid),
+         node <- %{node | id: id} do
+      {:ok, node}
+    end
+  end
+
+  def navigate(%DomNode{base_url: base_url, pid: pid} = node, path) do
+    event = "Page.domContentEventFired"
+
+    subscribe(node, event)
+
+    with {:ok, _} <- RPC.Page.enable(pid),
+         url <- URI.merge(base_url, path) |> to_string(),
+         {:ok, _} <- RPC.Page.navigate(pid, %{url: url}) do
+      receive do
+        {:chrome_remote_interface, event, _response} ->
+          unsubscribe(node, event)
+          {:ok, node}
+      after
+        2_000 ->
+          unsubscribe(node, event)
+          {:error, "page did not load after navigating to #{url}"}
+      end
+    end
+  end
+
+  def simulate_click(%DomNode{} = node) do
+    event = "Page.frameNavigated"
+
+    subscribe(node, event)
+
+    [x, y] = node |> Chrome.get_box_model() |> Quad.center()
+
+    node |> Chrome.dispatch_mouse_event("mouseMoved", x, y, "none")
+    node |> Chrome.dispatch_mouse_event("mousePressed", x, y, "left")
+    node |> Chrome.dispatch_mouse_event("mouseReleased", x, y, "left")
+
+    receive do
+      {:chrome_remote_interface, event, _response} ->
+        unsubscribe(node, event)
+        {:ok, node} = wait_for_and_get_document(node)
+        node
+    after
+      500 ->
+        unsubscribe(node, event)
+        {:error, "click did not result in a page navigation"}
+    end
+  end
+
+  def wait_for_and_get_document(%DomNode{} = node) do
+    event = "Page.domContentEventFired"
+
+    subscribe(node, event)
+
+    receive do
+      {:chrome_remote_interface, event, _response} ->
+        unsubscribe(node, event)
+        get_document(node)
+    after
+      2_000 ->
+        unsubscribe(node, event)
+        {:error, "page did not load"}
     end
   end
 
@@ -30,27 +100,13 @@ defmodule Click.Browser do
     end
   end
 
-  def navigate(%DomNode{base_url: base_url, pid: pid} = node, path) do
-    with {:ok, _} <- RPC.Page.enable(pid),
-         :ok <- PageSession.subscribe(pid, "Page.domContentEventFired"),
-         url <- URI.merge(base_url, path) |> to_string(),
-         {:ok, _} <- RPC.Page.navigate(pid, %{url: url}) do
-      receive do
-        {:chrome_remote_interface, "Page.domContentEventFired", _response} ->
-          :ok = PageSession.unsubscribe(pid, "Page.domContentEventFired")
-          {:ok, node}
-      after
-        2_000 ->
-          :ok = PageSession.unsubscribe(pid, "Page.domContentEventFired")
-          {:error, "Timeout waiting for Page.domContentEventFired for navigating to #{url}"}
-      end
-    end
+  #
+
+  defp subscribe(%DomNode{pid: pid}, event) do
+    :ok = PageSession.subscribe(pid, event)
   end
 
-  def get_document(%DomNode{pid: pid} = node) do
-    with {:ok, %{"result" => %{"root" => %{"nodeId" => id}}}} <- RPC.DOM.getDocument(pid),
-         node <- %{node | id: id} do
-      {:ok, node}
-    end
+  defp unsubscribe(%DomNode{pid: pid}, event) do
+    :ok = PageSession.unsubscribe(pid, event)
   end
 end
