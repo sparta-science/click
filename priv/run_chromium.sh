c53351b03d12b1a8e2d758827cbc73aa39e42f14
Erik Hanson and Kelly Felkins <pair+erik+kelly@spartascience.com>
2019-07-09 16:03:09 -0700

    remove unneeded browser manager and scripts
    [#166706845]

--- priv/run_chromium.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-port=${1:-9222}
-
-# Window size is set to 10,000px high so that all elements are visible and therefore clickable.
-# In the future, we should scroll to the element we want to click on before clicking on it.
-/Applications/Chromium.app/Contents/MacOS/Chromium --headless --remote-debugging-port=${port} --window-size="1024x10000"
