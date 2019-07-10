fda735cd37239b0463dc60da51c3e7bb9f11c7d2
Erik Hanson and Kelly Felkins <pair+erik+kelly@spartascience.com>
2019-07-10 15:02:08 -0700

    go back to newer docker image for circle

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Click.MixProject do
     [
       app: :click,
       deps: deps(),
-      elixir: "~> 1.6",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       version: "0.1.6"
